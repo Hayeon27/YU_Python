3b03e267d4094ea0f4538de6370ec85fe3074edc
Len_Somwhere <105035982+Hayeon27@users.noreply.github.com>
2022-06-20 04:33:40 +0900

    [소프트웨어와 인공지능] Homework 2.1 (원의 면적 및 둘레 계산)
    find the area and circumference of a circle

---
+++ Homework 2.1.py
@@ -0,0 +1,14 @@
+'''
+Project : Homework 2.1 (원의 면적 및 둘레 계산)
+Author : Len_somwhere
+Last Updated Date : June 20 2022
+'''
+import math                      #import math module for using math.pi
+
+rad = int(input("radius : "))    #rad : a value to be entered as radius
+
+area = math.pi * rad * rad       #area : the area of a circle (using the formula for the area of a circle)
+circum = 2 * math.pi * rad       #circum : the circumference of a circle (using the formula for the circumference of a circle)
+
+print("Circle of radius ({}) : area ({}), circumference ({})"\
+    .format(rad, area, circum))  #print "rad, area, circum"
