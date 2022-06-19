6376ec4e7d1492d4db97779953466534be4ce7bd
Len_Somwhere <105035982+Hayeon27@users.noreply.github.com>
2022-06-20 04:29:25 +0900

    Update Homework 2.1.py

--- Homework 2.1.py
+++ Homework 2.1.py
@@ -10,5 +10,5 @@ rad = int(input("radius : "))   #rad : a value to be entered as radius
 area = math.pi * rad * rad      #area : the area of a circle (using the formula for the area of a circle)
 circum = 2 * math.pi * rad      #circum : the circumference of a circle (using the formula for the circumference of a circle)
 
-print("Circle of radius ({}) : area ({}), circumference ({})")\
-    .format(rad, area, circum)  #print "rad, area, circum"
+print("Circle of radius ({}) : area ({}), circumference ({})"\
+    .format(rad, area, circum)) #print "rad, area, circum"
