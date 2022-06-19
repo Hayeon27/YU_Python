7b7ff0dad06a4fc1e9f69be42a4152b85521c9a5
Len_Somwhere <105035982+Hayeon27@users.noreply.github.com>
2022-06-20 04:34:30 +0900

    [소프트웨어와 인공지능] Homework 2.2 (직사각형의 면적 및 둘레 계산)
    find the area and perimeter of a rectangle

---
+++ Homework 2.2.py
@@ -0,0 +1,20 @@
+'''
+Project : Homework 2.2 (직사각형의 면적 및 둘레 계산)
+Author : Len_somwhere
+Last Updated Date : June 20 2022
+'''
+input_str = input("width, length : ")                 #input_str : a value to be entered as width and length
+
+width, length = map(int, input_str.split(sep = ' '))  #(1)
+
+area = width * length                                 #area : the area of a rectangle (using the formula of the area of a rectangle)
+peri = 2 * (width + length)                           #peri : the perimeter of a rectangle (using the formula of the perimeter of a rectangle)
+
+print("Rectangle of width ({}) and length ({}) : area ({}), perimeter ({})"\
+    .format(width, length, area, peri))               #print "width, length, area, peri"
+
+'''
+(1) : separate words based on spaces, convert them to int format, and store them in width and length variables
+ - width : the width of a rectangle
+ - length : the length of a rectangle
+'''
